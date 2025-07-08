7ac325bb73382fe3d0a96c1d39a855ee40cfe61a
sevvalokten <sevvalokten1@gmail.com>
2025-07-08 15:10:38 +0300

    first commit

---
+++ burclar_yorumu/lib/pages/burc_detay.dart
@@ -0,0 +1,12 @@
+import 'package:burclar_yorumu/models/burc.dart';
+import 'package:flutter/material.dart';
+
+class BurcDetay extends StatelessWidget {
+  final Burc secilenBurc;
+  const BurcDetay({super.key, required this.secilenBurc});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ burclar_yorumu/lib/widgets/burc_item.dart
@@ -0,0 +1,25 @@
+import 'package:burclar_yorumu/models/burc.dart';
+import 'package:flutter/material.dart';
+
+class BurcItem extends StatelessWidget {
+  final Burc gelenBurc;
+  const BurcItem({super.key, required this.gelenBurc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(4),
+      child: Card(
+        elevation: 4, //kart arkasına gölgelendirme yapar.
+        child: Padding(padding: EdgeInsets.all(8),
+        child: ListTile(
+          leading: Image.asset("images/${gelenBurc.burcKucukResim}"),
+          title: Text(gelenBurc.burcAdi, style: Theme.of(context).textTheme.headlineSmall ,),
+          subtitle: Text(gelenBurc.burcTarih, style: Theme.of(context).textTheme.bodyLarge,),
+          trailing: Icon(Icons.arrow_forward_ios, color: Theme.of(context).iconTheme.color,),
+        ),
+        ),
+      ),
+      );
+  }
+}
